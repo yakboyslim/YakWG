a8e9aa485433c4d97e782bc25acd8239afd47b32
yakboyslim <58483496+yakboyslim@users.noreply.github.com>
2023-04-05 23:39:13 -0400

    Preparing for first release

--- WG_Tuner_01.m
+++ WG_Tuner_01.m
@@ -1,12 +1,14 @@
+function WG_Tuner
+
 %% Read Log
 clear all
 close all
 
 %% Get Variable list
 
-varconv=readtable(fullfile(getcurrentdir,"variables.csv"),Delimiter=',',TextType='string',ReadVariableNames=false)
-varconv=table2array(varconv)
-varconv(:,1)=[]
+varconv=readtable(fullfile(getcurrentdir,"variables.csv"),Delimiter=',',TextType='string',ReadVariableNames=false);
+varconv=table2array(varconv);
+varconv(:,1)=[];
 
 %% Open Files
 
@@ -14,35 +16,34 @@ varconv(:,1)=[]
 
 if iscell(fileopen)
     log=readtable(fullfile(pathopen,string(fileopen(1))));
+    log=log(:,1:width(log)-1);
 else
     log=readtable(fullfile(pathopen,fileopen));
+    log=log(:,1:width(log)-1);
 end
 if iscell(fileopen)
     for i=2:length(fileopen)
-        log=vertcat(log,readtable(fullfile(pathopen,string(fileopen(i)))));
+        openlog=readtable(fullfile(pathopen,string(fileopen(i))));
+        openlog=openlog(:,1:width(openlog)-1);
+        log=vertcat(log,openlog);
     end
 end
 
-
-
-% export = questdlg('Do you want to open another .csv?','Open CSV','Yes','No','Yes');
-% while strcmpi(export,'Yes')
-%         [fileopen,pathopen]=uigetfile('*.csv','Select Log CSV File');
-%         log=vertcat(log,readtable(fullfile(pathopen,fileopen)));
-%         export = questdlg('Do you want to open another .csv?','Open CSV','Yes','No','Yes');
-% end
-
-
 %% Convert Variables
+logvars = log.Properties.VariableNames;
+for i=1:width(varconv)
+    if any(contains(logvars,varconv(1,i)))
+        log=renamevars(log,varconv(1,i),varconv(2,i));
+    end
+end
 
-log=renamevars(log,varconv(1,:),varconv(2,:));
+logvars = log.Properties.VariableNames;
 
 fudge=0.71;
 %% Create Derived Values
 log.deltaPUT=log.PUT-log.PUTSP;
-log.WGCL=log.WG_PD+log.WG_I
-log.WGNEED=log.WGPosBase___+log.WGCL-log.deltaPUT.*fudge
-log.flow=log.RPM.*log.Airmass_mg_stk_./(60*1222000)
+log.WGNEED=log.WG_Final-log.deltaPUT.*fudge;
+log.WGCL=log.WG_Final-log.WG_Base;
 
 %% Create Trimmed datasets
 
@@ -50,46 +51,31 @@ minpedal=50
 minboost=0
 maxdelta=10
 
-ACT=log;
-ACT(ACT.DV>50,:) = [];
-ACT(ACT.BOL>0,:) = [];
-ACT(ACT.TOL>0,:) = [];
-ACT(ACT.I_INH>0,:) = [];
-ACT(ACT.BOOST<minboost,:) = [];
-ACT(abs(ACT.deltaPUT)>maxdelta,:) = [];
-ACTO=ACT;
-ACTO(ACTO.WG_Final>98,:) = [];
-ACT_VVL1=ACTO;
-ACT_VVL1(ACT_VVL1.VVL~=1,:) = [];
-ACT_VVL0=ACTO;
-ACT_VVL0(ACT_VVL0.VVL~=0,:) = [];
-
-WOT=log;
-WOT(WOT.Pedal<minpedal,:) = [];
-WOT(WOT.DV>50,:) = [];
-WOT(WOT.BOOST<minboost,:) = [];
-WOT(abs(WOT.deltaPUT)>maxdelta,:) = [];
-WOTO=WOT;
-WOTO(WOTO.WG_Final>98,:) = [];
-WOT_VVL1=WOTO;
-WOT_VVL1(WOT_VVL1.VVL~=1,:) = [];
-WOT_VVL0=WOTO;
-WOT_VVL0(WOT_VVL0.VVL~=0,:) = [];
-
-
-
-%% Choose Best dataset
-USE=ACT
-USEO=ACTO
-USE_VVL1=ACT_VVL1
-USE_VVL0=ACT_VVL0
+USE=log;
+if any(contains(logvars,'BOL')) && any(contains(logvars,'TOL')) && any(contains(logvars,'I_INH'))
+    USE(USE.DV>50,:) = [];
+    USE(USE.BOL>0,:) = [];
+    USE(USE.TOL>0,:) = [];
+    USE(USE.I_INH>0,:) = [];
+else
+    USE(USE.Pedal<minpedal,:) = [];
+end
+    USE(USE.DV>50,:) = [];
+    USE(USE.BOOST<minboost,:) = [];
+    USE(abs(USE.deltaPUT)>maxdelta,:) = [];
+    USEO=USE;
+    USEO(USEO.WG_Final>98,:) = [];
+    USE_VVL1=USEO;
+    USE_VVL1(USE_VVL1.VVL~=1,:) = [];
+    USE_VVL0=USEO;
+    USE_VVL0(USE_VVL0.VVL~=0,:) = [];
 
 
 %% Read Axis Values
-exhaxis=csvread(fullfile(getcurrentdir,"exh_axis.csv"))
-intaxis=csvread(fullfile(getcurrentdir,"int_axis.csv"))
-exhlabels=string(exhaxis)
-intlabels=string(intaxis)
+exhaxis=csvread(fullfile(getcurrentdir,"exh_axis.csv"));
+intaxis=csvread(fullfile(getcurrentdir,"int_axis.csv"));
+exhlabels=string(exhaxis);
+intlabels=string(intaxis);
 
 %% Create Bins
 
@@ -109,39 +95,37 @@ end
 
 SUM1=zeros(length(intaxis),length(exhaxis));
 COUNT1=SUM1;
-SUM0=SUM1
-COUNT0=SUM1
+SUM0=SUM1;
+COUNT0=SUM1;
 
 %% Discretize VVL1
 
 X1=discretize(USE_VVL1.EFF,xedges);
 Y1=discretize(USE_VVL1.IFF,yedges);
 for i=1:height(USE_VVL1)
-   weight=USE_VVL1.deltaPUT(i)*(0.5-abs(USE_VVL1.EFF(i)-X1(i)))*(0.5-abs(USE_VVL1.IFF(i)-Y1(i)))
-%    weight=abs(USE_VVL1.deltaPUT(i))+0.1
+   weight=USE_VVL1.deltaPUT(i)*(0.5-abs(USE_VVL1.EFF(i)-X1(i)))*(0.5-abs(USE_VVL1.IFF(i)-Y1(i)));
    SUM1(Y1(i),X1(i))=SUM1(Y1(i),X1(i))+weight*USE_VVL1.WGNEED(i);
    COUNT1(Y1(i),X1(i))=COUNT1(Y1(i),X1(i))+weight;
 end
-AVG1=round(SUM1./COUNT1)/100
-Res_1=array2table(AVG1,'VariableNames',exhlabels,'RowNames',intlabels)
+AVG1=round(SUM1./COUNT1)/100;
+Res_1=array2table(AVG1,'VariableNames',exhlabels,'RowNames',intlabels);
 
 %% Discretize VVL0
 
 X0=discretize(USE_VVL0.EFF,xedges);
 Y0=discretize(USE_VVL0.IFF,yedges);
 for i=1:height(USE_VVL0)
-   weight=USE_VVL0.deltaPUT(i)*(0.5-abs(USE_VVL0.EFF(i)-X0(i)))*(0.5-abs(USE_VVL0.IFF(i)-Y0(i)))
-%    weight=abs(USE_VVL0.deltaPUT(i))+0.1
+   weight=USE_VVL0.deltaPUT(i)*(0.5-abs(USE_VVL0.EFF(i)-X0(i)))*(0.5-abs(USE_VVL0.IFF(i)-Y0(i)));
    SUM0(Y0(i),X0(i))=SUM0(Y0(i),X0(i))+(weight)*USE_VVL0.WGNEED(i);
    COUNT0(Y0(i),X0(i))=COUNT0(Y0(i),X0(i))+weight;
 end
-AVG0=round(SUM0./COUNT0)/100
-Res_0=array2table(AVG0,'VariableNames',exhlabels,'RowNames',intlabels)
+AVG0=round(SUM0./COUNT0)/100;
+Res_0=array2table(AVG0,'VariableNames',exhlabels,'RowNames',intlabels);
 
 %% Save Tables
 
-writetable(Res_1,fullfile(pathopen,"VVL1 Results.csv"),'WriteRowNames',true);
-writetable(Res_0,fullfile(pathopen,"VVL0 Results.csv"),'WriteRowNames',true);
+writetable(Res_1,fullfile(pathopen,"VVL1 WG Results.csv"),'WriteRowNames',true);
+writetable(Res_0,fullfile(pathopen,"VVL0 WG Results.csv"),'WriteRowNames',true);
 
 %% Plot Scatters
 
@@ -163,9 +147,6 @@ for i=1:height(USE)
         end
     end
 end
-  
-% lgd = [o1 o0];
-% leglabels = legend(lgd,'WG open - VVL1','WG open - VVL0');
 
 mycolormap = customcolormap([0 .25 .5 .75 1], {'#9d0142','#f66e45','#ffffff','#65c0ae','#5e4f9f'});
 c = colorbar;
@@ -201,9 +182,6 @@ for i=1:height(USE)
         end
     end
 end
-  
-% lgd = [o1p o0p];
-% leglabels = legend(lgd, 'WG open - VVL1','WG open - VVL0');
 
 mycolormap = customcolormap([0 .25 .5 .75 1], {'#9d0142','#f66e45','#ffffff','#65c0ae','#5e4f9f'});
 c = colorbar;
@@ -222,24 +200,7 @@ rows=[find(USE.RPM>2000,1) find(USE.RPM>3000,1) find(USE.RPM>4000,1) find(USE.RP
 lscatter(USE.EFF(rows),USE.IFF(rows),[2000 3000 4000 5000]);
 hold off
 
-f3=figure
-scatter3 (log.flow,log.PressRatio__,log.RPM,log.BOOST+25,log.RPM)
-hold on
-plot([0 0.01 0.01 0.02 0.02 0.03 0.03 0.04 0.05 0.1],[1.06 1.09 1.12 1.22 1.31 1.55 1.71 2.1 3.29 4])
-plot([0 0.01 0.01 0.02 0.02 0.03 0.03 0.04 0.05 0.1],[1.02 1.03 1.04 1.10 1.18 1.38 1.54 1.70 1.89 2.96])
-
-view(2)
-
-for i=2:height(log)-1
-    slope(i)=(log.flow(i)-log.flow(i-1))/(log.PressRatio__(i)-log.PressRatio__(i-1));
 end
-f4=figure;
-plot(slope,log.RPM(1:height(log)-1));
 
-% 
-% %% Plot Gplot
-% 
-% Xnames=["RPM" "Int Flow Factor" "Exh Flow Factor" "WG I" "WG PD" "WG Final" "PUT delta"];
-% comparison=[USEO.RPM USEO.IFF USEO.EFF USEO.WG_I USEO.WG_PD USEO.WG_Final USEO.deltaPUT];
-% f2=figure;
-% gplotmatrix(comparison,[],USEO.VVL,[],[],[],'on','stairs',Xnames);
+
+
